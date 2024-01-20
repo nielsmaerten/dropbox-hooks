189daf31e3a79300b15d8547ca6d991b55a26f2e
Niels Maerten <niels@maerten.be>
2024-01-20 11:23:31 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Dropbox Webhook Listener is running!"));
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
